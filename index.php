8f60988996c234accf720fcec5ed900ebf528789
induacharya <42159926+induacharya@users.noreply.github.com>
2019-03-05 00:59:50 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+error_reporting(1);
+error_reporting(E_ALL);
+
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: undefined");
+header("Access-Control-Allow-Headers: accept,authorization, content-type,Auth");
+header("Access-Control-Allow-Methods: PUT, GET, POST, DELETE, OPTIONS");
+
+require 'vendor/autoload.php';
+require 'phpmailer/PHPMailerAutoload.php';
+$app = new Slim\App();
+
+
+$app->add(function ($req, $res, $next) {
+    $response = $next($req, $res);
+   
+    return $response
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,Accept,Origin,Authorization,process-data,Auth');
+});
+
+
+
+
+require './api/utils.php';
+require './api/dbHandler.php';
+require './api/user.php';
+require './api/dbConnect.php';
+require './api/subject.php';
+//require './api/class.php' ;
+//require './api/studentRegistration.php';
+//require './api/Classsubmap.php' ;
+$app->run();
